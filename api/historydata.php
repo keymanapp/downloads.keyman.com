3671f0bf6b99cf2e0f86f0fb0f8b37c37b51f4e5
Darcy Wong <darcy_wong@sil.org>
2020-11-26 09:07:17 +0700

    fix: Simplify scope match so it doesn't  matter the order inside ()

--- api/historydata.php
+++ api/historydata.php
@@ -133,8 +133,8 @@ function filter_for_platform($contents, $platform) {
   $lines = preg_split("/(\r\n|\n|\r)/", $contents);
 
   // Grep lines that start with '#' | '*'
-  // scope might be separated by ','
-  $filtered_contents = preg_grep("/^(#|\*( )+($commit_types_regex)\(((.)*,)?($scopes_regex)\)).*$/", $lines);
+  // There might be multiple scopes within the '()'
+  $filtered_contents = preg_grep("/^(#|\*( )+($commit_types_regex)\(.*($scopes_regex).*\)).*$/", $lines);
   $filtered_contents = array_values(array_filter($filtered_contents));
 
   // Filter out intermittent releases where $platform wasn't updated
