30ac3f7dd9608b53436c9b548cdd7c3645cb5923
Marc Durdin <marc@durdin.net>
2020-12-14 12:38:12 +1100

    Revert "feat: sentry support for backend"

--- api/base.php
+++
@@ -1,6 +0,0 @@
-<?php
-  require_once __DIR__ . '/../vendor/autoload.php';
-  require_once __DIR__ . '/../_common/KeymanSentry.php';
-
-  const SENTRY_DSN = 'https://c997ea5ff5f04931b139ae1ec43e1d06@sentry.keyman.com/16';
-  \Keyman\Site\Common\KeymanSentry::init(SENTRY_DSN);
